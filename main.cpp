d3165e90a1b7b0535292d9321e7ed345cf42d8cf
jon0 <jono4728@gmail.com>
2013-08-25 00:34:57 +1200

    testing

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 25/08/2013
+ *      Author: remnanjona
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	cout << "sfd" << endl;
+	return 0;
+}
+
+
