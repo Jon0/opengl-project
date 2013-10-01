41c459eaa0c65b519bec2c8ce671677b7486aa38
jon0 <jono4728@gmail.com>
2013-10-01 22:27:10 +1300

    shader types

---
+++ shader/ShaderStructs.h
@@ -0,0 +1,31 @@
+/*
+ * ShaderStructs.h
+ *
+ *  Created on: 1/10/2013
+ *      Author: remnanjona
+ */
+
+#ifndef SHADERSTRUCTS_H_
+#define SHADERSTRUCTS_H_
+
+#include <glm/glm.hpp>
+
+namespace std {
+
+struct CameraProperties {
+	glm::mat4 P;
+	glm::mat4 V;
+	glm::mat4 M;
+};
+
+struct LightProperties {
+	glm::vec4 position;
+	glm::vec4 color;
+	glm::vec4 direction;
+	float intensity;
+	float spotlight;
+	float spotlightInner;
+};
+
+} /* namespace std */
+#endif /* SHADERSTRUCTS_H_ */
