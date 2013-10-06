d5eae752b173e1091a1c0f44a283ac8c7a535e37
jon0 <jono4728@gmail.com>
2013-10-07 03:31:17 +1300

    trees

---
+++ lighting/Tree.cpp
@@ -0,0 +1,21 @@
+/*
+ * Tree.cpp
+ *
+ *  Created on: 7/10/2013
+ *      Author: remnanjona
+ */
+
+#include "Tree.h"
+
+namespace std {
+
+Tree::Tree() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Tree::~Tree() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ lighting/TreeNode.h
@@ -0,0 +1,22 @@
+/*
+ * TreeNode.h
+ *
+ *  Created on: 7/10/2013
+ *      Author: remnanjona
+ */
+
+#ifndef TREENODE_H_
+#define TREENODE_H_
+
+namespace std {
+
+class TreeNode {
+	TreeNode *inner;
+
+public:
+	TreeNode();
+	virtual ~TreeNode();
+};
+
+} /* namespace std */
+#endif /* TREENODE_H_ */
