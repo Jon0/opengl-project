c221809d833030061d1664b1edbc0b370c344494
jon0 <jono4728@gmail.com>
2013-10-15 13:59:42 +1300

    sdfs

---
+++ geometry/GMesh.cpp
@@ -0,0 +1,21 @@
+/*
+ * GMesh.cpp
+ *
+ *  Created on: 15/10/2013
+ *      Author: remnanjona
+ */
+
+#include "GMesh.h"
+
+namespace std {
+
+GMesh::GMesh() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GMesh::~GMesh() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
